98399d8687a2e5f881d3b1b3e54d405dfa4a221a
Mohammed Ak Yerima <mcakyerima@gmail.com>
2021-03-16 00:02:53 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is awesome!</h1>
+      {/* header */}
+      {/* title  _ select dropdown*/}
+      {/* info box */}
+      {/* info box */}
+      {/* info box */}
+      {/* info box */}
+
+
+      {/* table */}
+      {/* grap */}
+      {/* map */}
+    </div>
+  );
+}
+
+export default App;
