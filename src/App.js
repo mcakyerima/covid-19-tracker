3a81b5e9f6907868f01736f7bfe7027b953ff4ec
Mohammed Ak Yerima <mcakyerima@gmail.com>
2021-03-18 23:44:39 +0100

    app header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
+import {React , useState} from 'react'
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
+  const [countries, setCountry] = useState([
+    "Borno", "kano" , "yobe" , "damaturu"
+  ]);
+  console.log("this is" , countries)
   return (
+
     <div className="App">
-      <h1>this is awesome!</h1>
+      <div className="app__header">
+        <h1>this it awesome</h1>
+        <FormControl className="app_dropdown">
+          <Select
+            variant= "outlined"
+            value="abc">
+              {/* map through coutry list and display a drop down component */}
+            {countries.map((country) => (
+                  <MenuItem value={country}>{country}</MenuItem>
+            ))}
+              {/* <MenuItem value="worldwide">WorldWide</MenuItem>
+              <MenuItem value="worldwi">coool</MenuItem>
+              <MenuItem value="worldwide">aawesome</MenuItem>
+              <MenuItem value="worldwide">great</MenuItem>
+              <MenuItem value="worldwide">rediculous</MenuItem>
+              <MenuItem value="worldwide">fucused</MenuItem>
+              <MenuItem value="worldwide">final</MenuItem> */}
+          </Select>
+
+        </FormControl>
+        
+      </div>
       {/* header */}
       {/* title  _ select dropdown*/}
       {/* info box */}
@@ -18,6 +49,6 @@ function App() {
       {/* map */}
     </div>
   );
-}
+            }
 
 export default App;
