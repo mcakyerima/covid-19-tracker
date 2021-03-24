ee0b6717b5e9acdb6c581bc90434b7b44827e487
Mohammed Ak Yerima <mcakyerima@gmail.com>
2021-03-24 23:59:40 +0100

    init'

--- src/Linegraph.js
+++ src/Linegraph.js
@@ -3,27 +3,35 @@ import { Line } from 'react-chartjs-2';
 
 function Linegraph() {
     const [data , setData] = useState({});
-    useEffect(() => {
-        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-        .then((response) => response.json())
-        .then((data) => {
-            //clever stn
-        })
-    }, []);
 
-    const buildChartData = async data => {
+    const buildChartData = async (data , caseType= "cases") => {
         const chartData = [];
         let lastDataPoint;
         data.cases.forEach(date => {
             if (lastDataPoint) {
                 
                 const newDataPoint  = {
-                    x: datek
-                    y:data['cases'][dare] - lastDataPoint
+                    x: date,
+                    y:data[caseType][date] - lastDataPoint
                 }
+                chartData.pugh(newDataPoint);
             }
+            lastDataPoint = data[caseType][date];
         })
-    }
+        console.log('this is chart data' , chartData)
+        return chartData;
+    };
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then((response) => response.json())
+        .then((data) => {
+            const chartData = buildChartData(data);
+            setData(chartData);
+            //clever stn
+        })
+    }, []);
+
+   
     buildChartData();
 
     return (
